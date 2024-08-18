0f9ee38b35997956014c84ee89d01c636e36dc0d
ChaneMo <91654630+ChaneMo@users.noreply.github.com>
2024-08-18 09:37:56 +0800

    Create web_address.py

---
+++ interactionWithPC/automation/web_address.py
@@ -0,0 +1,7 @@
+web_dic = {
+    'B站':'https://www.bilibili.com/',
+    '百度':'https://www.baidu.com',
+    '谷歌':'https://www.google.com',
+    '微博':'https://www.weibo.com',
+    'github':'https://github.com/'
+}
